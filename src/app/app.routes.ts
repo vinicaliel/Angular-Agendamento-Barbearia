2177604fba8490d0583bf23dcb41c3affd06fed5
Caliel Passos <caliel1023@yahoo.com>
2025-04-17 12:49:38 -0300

    feat: homepage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,10 @@ import { EditClientComponent } from './clients/edit-client/edit-client.component
 import { NewClientComponent } from './clients/new-client/new-client.component';
 import { ListClientsComponent } from './clients/list-client/list-client.component'
 import { SchedulesMonthComponent } from './schedules/schedules-month/schedules-month.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+    { path: '', component: HomeComponent, data: { title: 'Home' } }, // Rota padrão
     { path: 'clients/edit-client/:id', component: EditClientComponent, data: { title: 'Atualizar Cliente' } },
     { path: 'clients/new-client', component: NewClientComponent, data: { title: 'Cadastrar Cliente' } },
     { path: 'clients/list', component: ListClientsComponent, data: { title: 'Clientes Cadastrados' } },
